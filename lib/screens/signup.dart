89fc1089dcc23b10488c491e99c090d65070b404
Malak Alshamali <malakfaed99@gmail.com>
2021-09-26 11:11:09 +0300

    in the company

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -25,163 +25,165 @@ class _SignupState extends State<Signup> {
       backgroundColor: Colors.red[900],
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 300.0,
-              child: Image.asset('images/pizza.png'),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            TextField(
-              textAlign: TextAlign.left,
-              cursorColor: Colors.white70,
-              onChanged: (value) {
-                username = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter UserName',
-                hintStyle: TextStyle(color: Colors.white),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 20,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 300.0,
+                child: Image.asset('images/pizza.png'),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextField(
+                textAlign: TextAlign.left,
+                cursorColor: Colors.white70,
+                onChanged: (value) {
+                  username = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter UserName',
+                  hintStyle: TextStyle(color: Colors.white),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white70,
-                    width: 0.5,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white70,
+                      width: 0.5,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.person_add_alt_1_outlined,
                     color: Colors.white,
-                    width: 2,
+                    size: 20,
                   ),
-                ),
-                prefixIcon: Icon(
-                  Icons.person_add_alt_1_outlined,
-                  color: Colors.white,
-                  size: 20,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            TextField(
-              textAlign: TextAlign.left,
-              cursorColor: Colors.white70,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your Email',
-                hintStyle: TextStyle(color: Colors.white),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 20,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                textAlign: TextAlign.left,
+                cursorColor: Colors.white70,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your Email',
+                  hintStyle: TextStyle(color: Colors.white),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white70,
-                    width: 0.5,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white70,
+                      width: 0.5,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.email_outlined,
                     color: Colors.white,
-                    width: 2,
+                    size: 20,
                   ),
-                ),
-                prefixIcon: Icon(
-                  Icons.email_outlined,
-                  color: Colors.white,
-                  size: 20,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.left,
-              cursorColor: Colors.white70,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your Password',
-                hintStyle: TextStyle(color: Colors.white),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 20,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+              SizedBox(
+                height: 8,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.left,
+                cursorColor: Colors.white70,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your Password',
+                  hintStyle: TextStyle(color: Colors.white),
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 20,
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white70,
-                    width: 0.5,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white70,
+                      width: 0.5,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.password,
                     color: Colors.white,
-                    width: 2,
+                    size: 20,
                   ),
-                ),
-                prefixIcon: Icon(
-                  Icons.password,
-                  color: Colors.white,
-                  size: 20,
                 ),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Mybutton(
-              color: Colors.white,
-              title: 'SIGN UP',
-              onPressed: () async {
-                //print(username);
+              SizedBox(
+                height: 15,
+              ),
+              Mybutton(
+                color: Colors.white,
+                title: 'SIGN UP',
+                onPressed: () async {
+                  //print(username);
 
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  Navigator.pushNamed(context, HomePage.screenRoute);
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ],
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    Navigator.pushNamed(context, HomePage.screenRoute);
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
